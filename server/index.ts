ab671412431d52b3a716ff00dfab1c439aedff55
hariyebk <harunbekri6@gmail.com>
2024-10-22 11:06:41 +0300

    Initial setup

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,7 @@ import { Hono } from "hono";
 const app = new Hono();
 
 app.get("/", (c) => {
-  return c.text("Hello Harun Welcome to the British Council !");
+  return c.text("Hello Harun Welcome back");
 });
 
 export default app;
