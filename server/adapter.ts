1abe55cb4ee1e835f93e7b1d369da0367617a73c
hariyebk <harunbekri6@gmail.com>
2024-11-02 11:48:54 +0300

    lucia auth adapter

--- server/adapter.ts
+++ server/adapter.ts
@@ -1,6 +1,8 @@
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle"
 import {drizzle} from "drizzle-orm/postgres-js"
 import postgres from "postgres"
 import {z} from "zod"
+import { sessionTable, userTable } from "./db/schemas/auth"
 
 const EnvSchema = z.object({
     DATABASE_URL: z.string().url()
@@ -8,6 +10,13 @@ const EnvSchema = z.object({
 
 const processEnv = EnvSchema.parse(process.env)
 const queryClient = postgres(processEnv.DATABASE_URL)
-const db = drizzle(queryClient)
-const result = await db.execute("select 1");
-console.log(result)
+export const db = drizzle(queryClient, {
+    schema: {
+        user: userTable,
+        session: sessionTable
+    }
+})
+
+export const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable)
+// const result = await db.execute("select 1");
+// console.log(result)
